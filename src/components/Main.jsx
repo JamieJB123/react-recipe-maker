e2e4f03f5ae5cf8780bd87a140474007c54d2547
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-08-27 16:46:44 +0100

    Add logic to handle form submission: getting the form data and adding that new ingredient to an empty array of ingredients. This doesn't update the DOM / components yet as haven't learned how to interact with state yet

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,29 @@
 export default function Main() {
+
+    const ingredients = []
+
+    function onSubmitHandler(e) {
+        e.preventDefault();
+        console.log("Form submitted!");
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+    }
+
+    const ingredientElements = ingredients.map((item) => {
+        return <li>{item}</li>
+    })
+
+
     return (
         <main>
-            <form className="ingredient-form">
-                <input placeholder="e.g. oregano" aria-label="Add ingredient" type="text"></input>
+            <form onSubmit={onSubmitHandler}className="ingredient-form">
+                <input name="ingredient" placeholder="e.g. oregano" aria-label="Add ingredient" type="text"></input>
                 <button>+ Add ingredient</button>
             </form>
+            <ul>
+                {ingredientElements}
+            </ul>
         </main>
     )
 }

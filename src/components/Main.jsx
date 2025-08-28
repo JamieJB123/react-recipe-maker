53997815056e2e77adf54ddf3f30d1f90d080281
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-08-28 17:16:10 +0100

    Add the ingredients and recipe section with styling. Add conditional logic to display this section only if user has added incredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,16 +13,27 @@ export default function Main() {
         return <li key={item}>{item}</li>
     })
 
-
     return (
         <main>
             <form action={submitIngredient}className="ingredient-form">
                 <input name="ingredient" placeholder="e.g. oregano" aria-label="Add ingredient" type="text"></input>
                 <button>+ Add ingredient</button>
             </form>
-            <ul>
-                {ingredientElements}
-            </ul>
+            {ingredientElements.length > 0 &&
+            <section className="ingredients-section">
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">
+                    {ingredientElements}
+                </ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
+            }
         </main>
     )
 }
